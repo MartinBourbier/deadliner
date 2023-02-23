8bec080d9c4b9e583a7c3318a3a56bb1c05acc13
Martin Bourbier <martin.bourbier@epita.fr>
2023-02-23 12:23:54 +0100

    feat: token retrieval and basic builder

--- src/main/java/com/deadliner/Main.java
+++ src/main/java/com/deadliner/Main.java
@@ -1,7 +1,15 @@
 package com.deadliner;
 
+import net.dv8tion.jda.api.JDABuilder;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String token = System.getenv("DISCORD_BOT_TOKEN");
+        if (token == null) {
+            System.err.println("ERROR: bot token could not be retrieved through env. Did you set it properly?");
+            System.exit(1);
+        }
+        JDABuilder jdaBuilder = JDABuilder.createLight(token);
+        jdaBuilder.build();
     }
 }
