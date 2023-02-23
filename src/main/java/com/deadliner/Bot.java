799e4324f49fe251dacec0f3f4eae7d46cdb445d
Martin Bourbier <martin.bourbier@epita.fr>
2023-02-23 22:07:16 +0100

    feat(modal): basics

--- src/main/java/com/deadliner/Bot.java
+++ src/main/java/com/deadliner/Bot.java
@@ -1,11 +1,15 @@
 package com.deadliner;
 
+import com.deadliner.utils.CustomTextInput;
 import lombok.val;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.components.text.TextInput;
+import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
+import net.dv8tion.jda.api.interactions.modals.Modal;
 
 public class Bot extends ListenerAdapter {
     public static void main(String[] args) {
@@ -26,14 +30,25 @@ public class Bot extends ListenerAdapter {
 
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
-        // make sure we handle the right command
         if (event.getName().equals("register")) {
-            event.reply("Yolo registered!").queue();
-        }
-        else if (event.getName().equals("remove")) {
+            TextInput name = CustomTextInput.of("deadline-label", "Deadline label", TextInputStyle.SHORT,
+                    64, true, "Descriptive label for the deadline.");
+            TextInput date = CustomTextInput.of("deadline-date", "Deadline date", TextInputStyle.SHORT,
+                    64, true, "Date of the deadline with format 'mm/dd/yyyy'.");
+            TextInput time = CustomTextInput.of("deadline-time", "Deadline time", TextInputStyle.SHORT,
+                    64, true, "Time of the deadline with format 'hh:mm'.");
+            TextInput link = CustomTextInput.of("deadline-link", "Deadline link", TextInputStyle.SHORT,
+                    1024, false, "Link to the associated task.");
+            Modal modal = Modal.create("deadline-modal", "Deadline info")
+                    .addActionRow(name)
+                    .addActionRow(date)
+                    .addActionRow(time)
+                    .addActionRow(link)
+                    .build();
+            event.replyModal(modal).queue();
+        } else if (event.getName().equals("remove")) {
             event.reply("Yolo removed!").queue();
-        }
-        else if (event.getName().equals("edit")) {
+        } else if (event.getName().equals("edit")) {
             event.reply("Yolo edited!").queue();
         }
     }
